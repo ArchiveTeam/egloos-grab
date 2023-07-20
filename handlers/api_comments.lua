a13fd92f3cd624c11a261ed84fbd4e79f1add92b
OrIdow6 <68304414+OrIdow6@users.noreply.github.com>
2023-07-20 04:12:29 -0700

    Fix misc with comments

--- handlers/api_comments.lua
+++ handlers/api_comments.lua
@@ -5,16 +5,27 @@ local module = {}
 
 module.get_urls = function(file, url, is_css, iri)
     local json = JSON:decode(get_body())
-    -- If this error is sent, at a page with no more comments
-    if not (json["error"] and json["category"] == "system" and json["message"] == "[OMGClient::selectQuery][(DBGateException) DBGateContext::decodeHeader, invalid response, opcode 200, recv opcode 0, body -100%\t#(MysqlException) mysql_real_query, 1064 You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near '-97' at line 1]") then
-        for _, comment in pairs(json["comments"]) do
+    -- If this error is sent, at a page with no more comments; this grab script will only encounterer it if the last valid page has >50 comments (relatively rare)
+    if json["error"] and json["error"]["category"] == "system" and json["error"]["message"]:match"[OMGClient::selectQuery][(DBGateException) DBGateContext::decodeHeader, invalid response, opcode 200, recv opcode 0, body -100%\t#(MysqlException) mysql_real_query, 1064 You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near '-97' at line 1]" then
+        return
+    end
+
+    for _, comment in pairs(json["comment"]) do
+        if comment["comment_writer"] ~= nil then
             queue_request({url="https://api.egloos.com/v3/blog/" .. comment["comment_writer"] .. ".json"}, "comment_user", true)
-            queue_request({url=comment["comment_writer_thumbnail"]}, "resources", true)
-            process_html(comment["comment_content"])
             process_url(comment["comment_writer_url"])
         end
+        queue_request({url=comment["comment_writer_thumbnail"]}, "resources", true)
+        process_html(comment["comment_content"])
+    end
 
-        queue_request({url=current_options.url:gsub("page=(%d+)$", function(num) tostring(tonumber(num) + 1) end)}, current_handler, false)
+    -- Apparently comments are 100/page; this is the same logic as the independent grab
+    if #json["comment"] > 50 then
+        local newurl = current_options.url:gsub("(%d+)$", function(num) tostring(tonumber(num) + 1) end)
+        local account, post, page = current_options.url:match('^https?://api%.egloos%.com/(.+)/post/(%d+)/comment%.json%?page=(%d+)$')
+        local newpage = tostring(tonumber(page) + 1)
+        newurl = "https://api.egloos.com/" .. account .. "/post/" .. post .. "/comment.json?page=" .. newpage
+        queue_request({url=newurl}, current_handler, false)
     end
 end
 
--- handlers/api_post.lua
+++ handlers/api_post.lua
@@ -6,8 +6,9 @@ local module = {}
 module.get_urls = function(file, url, is_css, iri)
     local json = JSON:decode(get_body())["post"]
     if json["comment_count"] ~= "0" then
+        local domain, post_num = current_options.url:match('^https://api%.egloos%.com/(.+)/post/(%d+)%.json')
         -- TODO should we figure out comments/page and never get that last, erroring request? Or always get the first page regardless of # listed here?
-        queue_request("https://api%.egloos%.com/" .. domain .. "/post/" .. post_num .. "/comment.json?page=1", "api_comments", true)
+        queue_request({url="https://api.egloos.com/" .. domain .. "/post/" .. post_num .. "/comment.json?page=1"}, "api_comments", true)
     end
     process_html(json["post_content"])
 end
